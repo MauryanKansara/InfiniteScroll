26aab9b711b15ebb24526cdc508d5bf8d6ac3aa5
Mauryan Kansara <mauryankansaramk@gmail.com>
2023-05-28 00:49:35 +0530

    API setup

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const count = 10; // Number of images to get
 const apiKey = "vK8FhpG4c8LNWx_y6bPXsANVZBYc8KqBdpFtMQzJz8w";
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash API
+
+async function getPhotos() {
+  try {
+    const response = await fetch(apiURL);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getPhotos();
