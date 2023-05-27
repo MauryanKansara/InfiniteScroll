9fa0438f04c8927fa6308bbdffba0770a4e545c1
Mauryan Kansara <mauryankansaramk@gmail.com>
2023-05-28 00:15:07 +0530

    project setup

---
+++ app.js
@@ -0,0 +1,3 @@
+const count = 10; // Number of images to get
+const apiKey = "vK8FhpG4c8LNWx_y6bPXsANVZBYc8KqBdpFtMQzJz8w";
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
